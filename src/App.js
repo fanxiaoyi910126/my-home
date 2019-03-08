d658ba49075a78ccbcd89ec2329d3dbfc4df9c6b
fanxiaoyi <837938069@qq.com>
2019-03-08 21:48:06 +0800

    login功能初步

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
-import 'semantic-ui-css/semantic.min.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import './App.css'
+// 导入组件库的样式
+import 'semantic-ui-css/semantic.min.css'
 
 import Login from './Login'
-import Main from './Main'
+import Main from './module/Main'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
         <BrowserRouter>
           <Switch>
             <Route exact path="/" component={Login} />
             <Route path="/home" component={Main} />
-            <Redirect to="/" />
           </Switch>
         </BrowserRouter>
-      </div>
     )
   }
 }
--- src/Login.js
+++ src/Login.js
@@ -1,2 +1,83 @@
+import React from 'react'
+import axios from 'axios'
+// 导入需要的UI组件
+import { Icon, Button, Form, Divider } from 'semantic-ui-react'
+// 导入样式
+import './login.css'
 
-import { Icon, Button } from 'semantic-ui-react';
+class Login extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+  handleUsername = (event) => {
+    this.setState({
+      username: event.target.value
+    })
+  }
+  handlePassword = (event) => {
+    this.setState({
+      password: event.target.value
+    })
+  }
+  submit = async () => {
+    // 获取表单数据
+    // 调用接口进行身份验证 http://47.96.21.88:8086/users/login
+    // 服务器返回一个状态：如果登录成功会返回一个状态token
+    // 保存token信息到sessionStorage
+    // 跳转到主页页
+    // console.log(this.state.username)
+    // console.log(this.state.password)
+    let ret = await axios.post('http://47.96.21.88:8086/users/login',{
+      uname: this.state.username,
+      pwd: this.state.password
+    })
+    console.log(ret)
+  }
+  render() {
+    return (
+      <div className="login-container">
+        <div className="login-logo">
+          <Icon name="home" color="orange" size="massive" />
+        </div>
+        <div className="login-form">
+          <Form>
+            <Form.Input
+              icon="user"
+              required
+              size="big"
+              iconPosition="left"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleUsername}
+              placeholder="请输入用户名"
+            />
+            <Form.Input
+              icon="lock"
+              required
+              size="big"
+              iconPosition="left"
+              name="password"
+              value={this.state.password}
+              onChange={this.handlePassword}
+              placeholder="请输入密码"
+            />
+            <Button onClick={this.submit} fluid color="green">
+              登录
+            </Button>
+            <Divider horizontal>---</Divider>
+          </Form>
+        </div>
+        <div className="login-third">
+          <Icon name="wechat" color="green" size="big" />
+          <Icon name="qq" color="red" size="big" />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Login
